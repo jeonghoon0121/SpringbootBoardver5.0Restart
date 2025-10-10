035c544889a1f7bfd51f9a96c8d9d34a76c9810c
JeongHoon <jeonghoon@example.com>
2025-10-10 14:25:50 +0900

    a->b 는 성공 나머지 경우의 수 구상중

--- SpringbootComver1.1B/src/main/java/com/ohgiraffers/com/boardver5/BoardV5RestfulController.java
+++ SpringbootComver1.1B/src/main/java/com/ohgiraffers/com/boardver5/BoardV5RestfulController.java
@@ -33,6 +33,16 @@ public class BoardV5RestfulController {
         response.put("message", name + "님, 서버에서 데이터를 받았습니다.");
         return ResponseEntity.ok(response);
     }
+    @PostMapping("/user")
+    public ResponseEntity<Map<String, String>> receiveUser(@RequestBody Map<String, String> payload) {
+        String name = payload.get("name");
+        service.addBoard(name); // 이름 저장
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", name + "님, 서버에서 데이터를 받았습니다.");
+        return ResponseEntity.ok(response);
+    }
+
 
     // 저장된 모든 사용자 이름 목록 반환
     @GetMapping("/all")
